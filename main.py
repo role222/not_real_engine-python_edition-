4a137f0ba44077f3c97e5b4473aeb8524000461e
role222 <113473005+role222@users.noreply.github.com>
2024-06-14 09:30:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
-back = cv2.imread("background.png", -1)
-art = cv2.imread("art.png", -1)
-im_out = back
-def img_obed(img, sa, sb):
+back = cv2.imread("background.png", -1)#back image
+art = cv2.imread("art.png", -1)#front image
+im_out = back#image that will be showed
+def img_obed(img, sa, sb):#first - image formate RGBA, second obsed y, third - obsed x
     for a in range(img.shape[0]):
         for b in range(img.shape[1]):
             if(img.shape[2] == 4):
@@ -15,8 +15,8 @@ def img_obed(img, sa, sb):
                 im_out[a + sa][b + sb][1] = img[a][b][1]
                 im_out[a + sa][b + sb][2] = img[a][b][2]
     pass
-img_obed(art, 100, 500)
+img_obed(art, 100, 500)#use function for merge image in one
 while 1:
-    cv2.imshow("lol", im_out)
+    cv2.imshow("lol", im_out)#show image
     if cv2.waitKey(1) & 0xFF == 27:
         break
